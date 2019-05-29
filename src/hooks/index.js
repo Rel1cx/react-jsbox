d950bbdcd9ffc40ef998740ba6a71147557095cc
Eva1ent <forseen@icloud.com>
2019-05-29 23:52:38 +0800

    add useCache useDebounce

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,13 @@
 import usePing from './usePing'
+import useCache from './useCache'
 import useMotion from './useMotion'
 import useLocation from './useMotion'
+import useDebounce from './useDebounce'
 
 export default {
   usePing,
+  useCache,
   useMotion,
-  useLocation
+  useLocation,
+  useDebounce
 }
