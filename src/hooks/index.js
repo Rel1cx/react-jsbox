0964a918b96ffc0ff7a4801122830539d540377d
Nicify <nsnull@protonmail.com>
2020-05-31 01:02:50 +0800

    chore: update dependencies

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,9 @@
 import useCache from './useCache'
 import useDebounce from './useDebounce'
 import useEventHandler from './useEventHandler'
-import useKeyboardHeight from './useKeyboardHeight'
 
 export default {
   useCache,
   useDebounce,
-  useEventHandler,
-  useKeyboardHeight
+  useEventHandler
 }
