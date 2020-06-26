d25e520283ddd74ac5886ae204a0fc76b08b72a5
Nicify <nsnull@protonmail.com>
2020-06-26 13:49:37 +0800

    feat: add UIView animation support

--- src/components/view.js
+++ src/components/view.js
@@ -1,15 +1,18 @@
 export default class View {
   constructor(type, props) {
-    const { layout, events } = props
+    const { layout, events, animate } = props
     this._element = $ui.create({
       type,
       props,
       layout,
       events
     })
+    this._animate = animate
   }
 
   _element = null
+
+  _animate = null
 
   get element() {
     return this._element
@@ -31,6 +34,21 @@ export default class View {
 
   update(updatePayload) {
     const element = this.element
+    if (this._animate) {
+      const { duration = 0.4, damping = 0, velocity = 0, options = 0, completion = () => { } } = this._animate
+      return $ui.animate({
+        duration,
+        animation() {
+          Object.keys(updatePayload).forEach(prop => {
+            element[prop] = updatePayload[prop]
+          })
+        },
+        damping,
+        velocity,
+        options,
+        completion
+      })
+    }
     Object.keys(updatePayload).forEach(prop => {
       element[prop] = updatePayload[prop]
     })
