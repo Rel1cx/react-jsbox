129b77e10adb01bd45fcb9ebc3d197acf259b3e0
Nicify <nsnull@protonmail.com>
2020-07-05 19:34:29 +0800

    refactor: use microbundle for bundle

---
+++ src/cjs.js
@@ -0,0 +1,3 @@
+import * as ReactJSBox from './index.js'
+if (typeof module < 'u') module.exports = ReactJSBox
+else self.ReactJSBox = ReactJSBox
--- src/components/view.js
+++ src/components/view.js
@@ -33,15 +33,13 @@ export default class View {
 
   update(updatePayload) {
     const element = this.element
-
     if (hasOwnProperty.call(updatePayload, 'animate')) {
       this._animate = updatePayload.animate
       delete updatePayload.animate
     }
-
     if (this._animate) {
       const {duration = 0.4, damping = 0, velocity = 0, options = 0, completion = () => {}} = this._animate
-      $ui.animate({
+      return $ui.animate({
         duration,
         animation() {
           Object.keys(updatePayload).forEach(prop => {
@@ -53,36 +51,9 @@ export default class View {
         options,
         completion
       })
-      this.showOverlay()
-      return
     }
     Object.keys(updatePayload).forEach(prop => {
       element[prop] = updatePayload[prop]
     })
-    this.showOverlay()
-  }
-
-  showOverlay() {
-    if (!global.__REACT_JSBOX_HIGHLIGHT_UPDATES__) {
-      return
-    }
-    const {cornerRadius, smoothCorners, size} = this.element
-    const overlayView = $ui.create({
-      type: 'view',
-      props: {
-        frame: $rect(0, 0, size.width, size.height),
-        alpha: 0.8,
-        cornerRadius,
-        smoothCorners,
-        bgcolor: $color('clear'),
-        borderColor: $color('#37afa9'),
-        borderWidth: 2,
-        userInteractionEnabled: false
-      }
-    })
-    this.element.add(overlayView)
-    setTimeout(() => {
-      overlayView.remove()
-    }, 300)
   }
 }
